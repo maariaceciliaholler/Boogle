a5e0df27853be416b7a6eafe6abe1235666cc1ce
maariaceciliaholler <mariaceciliaholler@gmail.com>
2024-04-06 18:45:59 -0300

    feat(login/register): add login and register

--- Boogle/app/build.gradle.kts
+++ Boogle/app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.firebase.database)
+    implementation(libs.androidx.activity)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
