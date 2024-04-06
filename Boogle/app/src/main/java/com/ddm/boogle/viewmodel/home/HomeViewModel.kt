835229d3b59a57e079b67fbb299e014aadbf516e
luíza <luiza@Luizas-MacBook-Air.local>
2024-04-06 16:56:31 -0300

    feat(books): add input to user book search

--- Boogle/app/src/main/java/com/ddm/boogle/viewmodel/home/HomeViewModel.kt
+++ Boogle/app/src/main/java/com/ddm/boogle/viewmodel/home/HomeViewModel.kt
@@ -3,11 +3,25 @@ package com.ddm.boogle.viewmodel.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Pesquise por um título"
     }
     val text: LiveData<String> = _text
+
+    fun searchBookByTitle(title: String) {
+        val apiKey = "AIzaSyBtPCOdgWYKmWEQP4Wh4Z8zyo9yiuM0NGk"
+        val query = title.replace(" ", "+")
+        val url = URL("https://www.googleapis.com/books/v1/volumes?q=$query&key=$apiKey")
+    }
 }
